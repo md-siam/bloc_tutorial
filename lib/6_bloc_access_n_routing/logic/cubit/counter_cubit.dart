ff1a68055fbbf2f3076e0f63ac977d764fb37ef4
Md. Siam <md.siam03@gmail.com>
2022-03-25 16:48:32 +0600

    bloc access & routing

--- lib/6_bloc_access_n_routing/logic/cubit/counter_cubit.dart
+++ lib/6_bloc_access_n_routing/logic/cubit/counter_cubit.dart
@@ -6,8 +6,16 @@ class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counterValue: 0, wasIncremented: true));
 
   void increment() => emit(
-      CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
+        CounterState(
+          counterValue: state.counterValue + 1,
+          wasIncremented: true,
+        ),
+      );
 
-  void decrement() => emit(CounterState(
-      counterValue: state.counterValue - 1, wasIncremented: false));
+  void decrement() => emit(
+        CounterState(
+          counterValue: state.counterValue - 1,
+          wasIncremented: false,
+        ),
+      );
 }
