09ba31af253f5cff29d9f8d038939f75d4029be5
md-siam <md.siam03@gmail.com>
2022-08-18 20:41:58 +0600

    doc: nothing

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(title: 'Flutter Demo'),
+      home: const MyHomePage(title: 'Counter App'),
     );
   }
 }
