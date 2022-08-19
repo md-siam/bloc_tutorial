c42bd8e69cae9d2a6e27d839f838b45c92cf15bf
md-siam <md.siam03@gmail.com>
2022-08-20 03:10:59 +0600

    main.dart files created for seperate applications

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bloc_tutorial/main.dart';
+import 'package:bloc_tutorial/4_weather_app/main_weather_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
