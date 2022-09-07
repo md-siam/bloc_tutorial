8e1331b6decd83bd1a81ab134f5797bf601add29
md-siam <md.siam03@gmail.com>
2022-09-07 11:50:44 +0600

    chor: simple modification

---
+++ lib/3_todo_app/1_todo_cubit_streamsubscription/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('Home Page'),
+          ],
+        ),
+      ),
+    );
+  }
+}
