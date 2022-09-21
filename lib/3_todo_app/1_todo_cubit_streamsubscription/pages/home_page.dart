0abd6225b0ea3f942bef5b126edcb8f8c29b274b
md-siam <md.siam03@gmail.com>
2022-09-21 12:52:56 +0600

    chor: starting

--- lib/3_todo_app/1_todo_cubit_streamsubscription/pages/home_page.dart
+++ lib/3_todo_app/1_todo_cubit_streamsubscription/pages/home_page.dart
@@ -6,12 +6,11 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Page')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
-            Text('Home Page'),
+            Text('TODOS'),
           ],
         ),
       ),
